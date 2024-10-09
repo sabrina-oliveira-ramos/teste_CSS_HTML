674493016133117b7b701e4809065821241b2757
sabrina-oliveira-ramos <ramossabrina174174@gmail.com>
2024-10-09 09:44:44 -0300

    Teste de CSS e HTML

---
+++ teste_CSS_HTML/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slide = document.querySelector('.slide');
+    const slides = document.querySelectorAll('.slide-item');
+    let index = 0;
+    let intervalo;
+
+    function mostrarSlide(n) {
+        slides.forEach((slide, i) => {
+            slide.classList.remove('active', 'prev', 'next');
+            if (i === n) slide.classList.add('active');
+            else if (i === n - 1) slide.classList.add('prev');
+            else if (i === n + 1) slide.classList.add('next');
+        });
+    }
+
+    function proximoSlide() {
+        index = (index + 1) % slides.length;
+        mostrarSlide(index);
+    }
+
+    function slideAnterior() {
+        index = (index - 1 + slides.length) % slides.length;
+        mostrarSlide(index);
+    }
+
+    function iniciarSlide() {
+        intervalo = setInterval(proximoSlide, 5000);s
+    }
+
+    function pararSlide() {
+        clearInterval(intervalo);
+    }
+
+    iniciarSlide(); 
+});
